1cc4e458af9aa5d43f6a544b0e67dbe07127cd27
@JELTech <jelt.tech@gmail.com>
2025-03-16 23:52:21 -0300

    Uso de Join

--- ConsultaInicial.sql
+++ ConsultaInicial.sql
@@ -1,3 +1,10 @@
 select * from customers;
 select * from orders;
 select * from shippings;
+
+-- Consulta 2
+SELECT c.customer_id as ID,  c.first_name as Nombre, c.last_name as Apellido, c.age as Edad, c.country as Pais,
+	   o.item as Producto, o.amount as Valor, s.status as 'Estado de Pedido'
+from customers c
+JOIN orders o ON o.customer_id=c.customer_id
+JOIN shippings s ON s.customer_id=c.customer_id;
